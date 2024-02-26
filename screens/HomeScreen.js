8fbb31282662630f442de77734090dae2445d20c
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2024-02-27 00:36:28 +0530

    added HomeScreen.js

---
+++ screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import {ScrollView, StyleSheet, View} from "react-native";
+import MainHeader from "../components/main-header/MainHeader";
+import HomeFilterBar from "../components/home-filter-bar/HomeFilterBar";
+import ProductCardContainer from "../components/product-card-container/ProductCardContainer";
+
+const HomeScreen = () => {
+
+    return (
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={{alignItems: "center"}} style={{width: '100%'}}>
+                <MainHeader/>
+                <HomeFilterBar/>
+                <ProductCardContainer/>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: '#fffff0',
+        backgroundColor: '#f3f3f3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default HomeScreen;
